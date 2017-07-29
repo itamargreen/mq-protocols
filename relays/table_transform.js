e50a06c60fea2f52cb9c57c6e26b6c5db36176e3
Idan Stark <idanstark42@gmail.com>
2017-07-29 13:33:16 +0300

    Compiles the last change

--- relays/table_transform.js
+++ relays/table_transform.js
@@ -23,7 +23,7 @@ function default_1(msg) {
         header: [RANK, NUMBER, NAME, TOP].concat(rounds),
         data: msg.data.ranking.map(function (rank, index) {
             var scores = rank.scores.map(function (score) { return score || 0; });
-            return [index, rank.team.number, rank.team.name, rank.highest].concat(scores);
+            return [rank.rank, rank.team.number, rank.team.name, rank.highest].concat(scores);
         })
     };
     return new mhub_1.Message('table:setData', data);
